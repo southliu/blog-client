e5a43b8c711d32b0543dfd3d0e6a27275182d39b
southliu <1275093225@qq.com>
2024-02-03 22:20:59 +0800

    wip: 抽离vite插件

--- packages/web-client/vite.config.ts
+++ packages/web-client/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { createVitePlugins } from '@south-blog/utils';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: createVitePlugins(),
   server: {
     open: true
   }
