922360a6c8eb5913c10eaaf607719630c7d65818
southliu <1275093225@qq.com>
2024-02-03 19:09:50 +0800

    feat: eslint

---
+++ apis/src/user.ts
@@ -0,0 +1,7 @@
+import { request } from './utils';
+
+export const userApi = {
+    getUser: (id: number) => request.get(`/api/users/${id}`),
+    createUser: (data: Record<string, unknown>) => request.post(`/api/users`, data),
+    getUserById: (id: number) => Promise.resolve(id),
+};
--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { userApi } from '@south-blog/apis';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     userApi.getUser(1).then(res => {
-      console.log('getUser:', res)
+      console.log('getUser:', res);
     });
     userApi.getUserById(2).then(res => {
-      console.log('getUserById:', res)
+      console.log('getUserById:', res);
     });
-  }, [])
+  }, []);
 
   return (
     <>
@@ -39,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
