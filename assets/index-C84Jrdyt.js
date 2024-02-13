f53d17b7ac41d228a3e7b24084025ab69f8a28a4
southliu <1275093225@qq.com>
2024-02-13 20:23:00 +0800

    perf: 去除多余文件

--- assets/index-C84Jrdyt.js
+++
@@ -1,1 +0,0 @@
-import{_ as f}from"./app-CS2U0jUD.js";export{f as default};
--- packages/h5-client/src/main.ts
+++ packages/h5-client/src/main.ts
@@ -2,7 +2,7 @@ import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
 import routes from '~pages'
 import 'uno.css';
-import './style.css'
+import '../../../shared/styles/index.less';
 
 export const createApp = ViteSSG(App, {
   base: import.meta.env.BASE_URL,
